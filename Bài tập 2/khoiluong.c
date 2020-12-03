94ef806765f64c2c9f3a020076a3919af00c8f04
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2020-12-03 10:43:42 +0700

    Add files via upload

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,13 @@
+  
+#include <stdio.h>
+
+int main(void) {
+  int m;
+  printf("nhap m =");
+  scanf("%d", &m);
+  printf("kg = %dg\n", m * 1000);
+  printf("KG  | G\n");
+  printf("%d", m);
+  printf("   |%d", m * 1000);
+  return 0;
+}
