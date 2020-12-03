5aaf20299750e2517154f3e19fe239fcf0e8c78f
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2020-12-03 08:44:09 +0700

    Add files via upload

---
+++ Bài tập 1/hinhtron.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao bien DT,CV,R 
+  double DT,CV,R;
+  // gan gia tri bien R
+  R = 1.5;
+  // khai bao hang PI kieu double
+  const double PI = 3.14;
+  // gan gia tri DT,CV
+  DT = R*R*PI;
+  CV = 2*R*PI;
+  // hien thi R, Cv, DT ra man hinh
+  printf("CV\t DT\n");
+  printf("%g\t  %g",CV,DT);
+  getchar();
+  return 0;
+}
