3aeae293b67125dfb7297b6b664f109352f93e34
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2020-12-10 08:57:56 +0700

    Add files via upload

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	scanf("%d",&a);
+	if(a>0){
+		printf("%d la so duong",a);
+}else if(a == 0){
+	printf("a = 0");
+}else{
+	printf("%d la so am",a);
+}
+
+ 	
+getchar();
+return 0;
+}
+
+
