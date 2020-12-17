04730843329ef28f3fd9dce0164c63a76ed72d0c
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2020-12-17 09:33:13 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,27 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	
-	//khai bao bien soluong, dongia, thanhtien
-	int soluong, dongia, thanhtien;
-	
-	//nhap gia tri don gia
-	printf("nhap dongia =");
-	scanf("%d", &dongia);
-	
-	//nhap gia tri soluong
-	printf("nhap soluong =");
-	scanf("%d", &soluong);
-	
-	thanhtien = dongia * soluong;
-	
-	//mua 5 sp giam 50%
-	if(soluong>=5){
-		if(dongia>=500000)
-		printf("so tien can thanh toan la %d", thanhtien*50/100);
-	}else{
-		printf("so tien can thanh toan la %d", thanhtien);
-	}
-	
-return 0;	
+int main(void) {
+int dongia, soluong, thanhtien;
+// nhập các biến cần thiết
+  printf("nhập đơn giá\n");
+// hiển thị ra màn hình dòng nhập đơn giá
+scanf("%d", &dongia);
+  printf("nhập số lượng\n");
+// hiển thị ra màn hình dòng nhập số lượng
+scanf("%d", &soluong);
+// dùng lệnh scanf để gán giá trị cho biến
+thanhtien = dongia * soluong;
+if (soluong >= 5 && dongia >= 500){
+  printf("số tiền cần phải thanh toán là %d", thanhtien * 50/100);
+// nếu số lượng mua nhỏ hơn lơn hơn hoặc bằng 5 và đơn giá lơn hơn hoặc băng 500 thì sẽ giảm giá 50%
+}else{
+  printf("số tiền cần phải thanh toán là %d", thanhtien);
+// Ngược lại số lượng nhỏ hơn 5 thì sẽ không giảm giá
+}
+  return 0;
 }
