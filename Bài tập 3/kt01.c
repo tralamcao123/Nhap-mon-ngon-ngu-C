df7907bcb7987b88e31bc51b9a34c55ca4590366
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-07 11:13:43 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
-
-int main(void) {
- int a, b, c;
- a = 1; b = 2; c = 3;
-  printf("====Bang cuu chuong so 2====\n");
-  printf("%d * 1 = %d\n", a, a * 1);
-  printf("%d * 2 = %d\n", a, a * 2);
-  printf("%d * 3 = %d\n", a, a * 3);
-  printf("====Bang cuu chuong so 3====\n");
-  printf("%d * 1 = %d\n", b, b * 1);
-  printf("%d * 2 = %d\n", b, b * 2);
-  printf("%d * 3 = %d\n", b, b * 3);
-   printf("====Bang cuu chuong so 4====\n");
-  printf("%d * 1 = %d\n", c, c * 1);
-  printf("%d * 2 = %d\n", c, c * 2);
-  printf("%d * 3 = %d\n", c, c * 3);
-  return 0;
+#include<stdio.h>
+int main (){
+  int a, b, c;
+  a = 2, b = 3, c = 4;
+  printf("Bang Cuu Chuong 2\n");
+  //hiển thị bảng cưu chương 2 ra màn hình dể dễ nhìn hơn
+  for (int i = 1; i <= 10; i++){
+  //vòng lặp bắt đầu từ 1 đén 10 và tăng dần 1 giá trị
+    printf("   %d * %d = %d\n", a, i, a * i);
+    //a * i và i tăng dần 1 giá trị và hiển thị ra màn hình
+    //Các dòng dưới tương tự
+  }
+  printf("Bang Cuu Chuong 3\n");
+  for (int i = 1; i <= 10; i++){
+    printf("   %d * %d = %d\n", b, i, b * i);
+  }
+  printf("Bang Cuu Chuong 4\n");
+  for (int i = 1; i <= 10; i++){
+    printf("   %d * %d = %d\n", c, i, c * i);
+  }
+    return 0; 
 }
