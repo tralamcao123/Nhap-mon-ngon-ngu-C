24d176175662a4fd8ff3fbcda93249b8e32f2ad2
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-07 07:51:27 +0700

    Create kt_07.c

---
+++ kt_07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{           //Khai bao bien kieu int 
+    int a, b, c;
+            //Hien thi ra man hinh
+            printf("Nhap a =\n");
+            //Nhap so lieu tu ban phim
+            scanf("%d", &a);
+            //Hien thi ra man hinh
+            printf("Nhap b =\n");
+            //Nhap so lieu tu ban phim
+            scanf("%d", &b);
+            //Hien thi ra man hinh
+            printf("Nhap c =\n");
+            //Nhap so lieu tu ban phim
+            scanf("%d", &c);
+    //xac dinh a, b, c co phai la ba canh cua tam giac khong        
+    if((a - c) < b && b < (a + c))
+            {
+            printf("La 3 canh cua tam giac");
+            }
+    else
+            {
+            printf("Khong phai la 3 canh cua tam giac");
+            }
+return 0;
+}
