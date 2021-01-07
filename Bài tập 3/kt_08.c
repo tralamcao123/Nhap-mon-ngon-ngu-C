09ac933aa09b7301e1d207893453c4c1f46c8d3e
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-07 07:59:36 +0700

    Create kt_08.c

---
+++ Bài tập 3/kt_08.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+        //Khai bao bien kieu float
+    float a, b;
+        //Hien thi ra man hinh so thuc thu 1
+        printf("Nhap vao so thuc thu nhat:\n");
+        //Nhap mot so tu ban phim
+        scanf("%f",&a);
+        //Hien thi ra man hinh so thuc thu 2
+        printf("Nhap vao so thuc thu hai:\n");
+        //Nhap mot so tu ban phim
+        scanf("%f",&b);
+        //Tinh tong
+    float tong = a * b;
+        //Vong lap if cung dau
+    if (tong > 0)
+        {
+        printf("%0.1f va %0.1f cung dau", a, b);
+        }
+        //Vong lap else if trai dau
+    else if (tong < 0)
+        {
+        printf("%0.1f va %0.1f trai dau", a, b);
+        }
+return 0;
+}
