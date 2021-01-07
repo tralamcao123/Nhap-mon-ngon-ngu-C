b5dd1eb02fe5011da9c750b1ce375242aa5bc2b7
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-07 08:25:21 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -1,52 +1,40 @@
 #include <stdio.h>
 
-int main(void) {
-  int n;
-  printf("nhap so n =");
-  //in ra dong nhap so n
-  scanf("%d", &n);
-  //nhap so n
-if (n <= 1 )
-{
-	//hien thi chu tieng anh ra  man hinh 
-	printf("one",n);
-}	
-else if (n <= 2)
-{	
-	printf("two",n);
-}
-else if (n <= 3)
-{
-	printf("three",n);
-}
-else if (n <= 4)
-{
-	printf("four",n);
-}
-else if (n <= 5)
-{
-	printf("five",n);
-}
-else if (n <= 6)
-{
-	printf("six",n);
-}
-else if (n <= 7)
-{
-	printf("seven",n);
-}
-else if (n <= 8)
-{
-	printf("eight",n);
-}
-else if (n <= 9)
-{
-	printf("nine",n);
-}
-else if (n > 9)
-{
-	printf("greater than 9",n);
-}
-
-  return 0;
+int main() {
+    
+    int n;
+	
+    scanf("%d", &n);
+    if(n == 1) {
+        printf("one");
+    }
+    else if(n == 2) {
+        printf("two");
+    }
+    else if(n == 3) {
+        printf("three");
+    }
+    else if(n == 4) {
+        printf("four");
+    }
+    else if(n == 5) {
+        printf("five");
+    }
+    else if(n == 6) {
+        printf("six");
+    }
+    else if(n == 7) {
+        printf("seven");
+    }
+    else if(n == 8) {
+        printf("eight");
+    }
+    else if(n == 9) {
+        printf("nine");
+    }
+    else {
+        printf("Greater than 9");
+    }
+    
+    return 0;
 }
