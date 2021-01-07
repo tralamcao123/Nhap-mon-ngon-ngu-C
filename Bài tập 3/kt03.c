97e23df10ac22d604c9044a2706c46bae6804fb2
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-07 07:36:10 +0700

    Update kt03.c

--- Bài tập 3/kt03.c
+++ Bài tập 3/kt03.c
@@ -7,24 +7,23 @@ int main()
     printf("Cac so le tu 1 den 20 tru so 5, 11, 15\n");
 	//Vong lap so le tu 1 den 20 tru so 5, 11, 15
 	for (i = 1; i < 4; i = i + 2)
-{	
-		//Hien thi ra man hinh
-		printf("%d ", i);
+{	//Hien thi ra man hinh
+	printf("%d ", i);
 }
 for (i = 7; i <11 ; i = i + 2)
 {	
-		//Hien thi ra man hinh
-		printf("%d ", i);
+   	//Hien thi ra man hinh
+	printf("%d ", i);
 }
 for (i = 13; i < 15; i = i + 2)
 {	
-		//Hien thi ra man hinh
-		printf("%d ", i);
+	//Hien thi ra man hinh
+	printf("%d ", i);
 }
 for (i = 17; i < 21; i = i + 2)
 {	
-		//Hien thi ra man hinh
-		printf("%d ", i);
+	//Hien thi ra man hinh
+	printf("%d ", i);
 }
   
 return 0;
