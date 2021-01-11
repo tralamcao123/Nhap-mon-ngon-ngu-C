bfcf155cdf0503cdaabcfa5449f99d0d2b52347d
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-11 10:00:08 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -1,30 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) {
-    //h = 4: 4 hang
-    //hang 1: 1 ngoi sao 6 khoang trang
-    //hang 2: 3 ngoi sao 4 khoang trang
-    //hang 3: 5 ngoi sao 2 khoang trang
-    //hang 4: 7 ngoi sao 0 khoang trang
-    
-     //h = 3: 3 hang
-    //hang 1: 1 ngoi sao 4 khoang trang
-    //hang 2: 3 ngoi sao 2 khoang trang
-    //hang 3: 5 ngoi sao 0 khoang trang
-    //hang 4:0 ngoi sao 0 khoang trang
-    
-     //h = 2: 2 hang
-    //hang 1: 1 ngoi sao 2 khoang trang
-    //hang 2:3 ngoi sao 0  khoang trang
-    //hang 3:? ngoi sao ? khoang trang
-    //hang 4:? ngoi sao ? khoang trang
-    
-        //h = 5: 5 hang
-    //hang 1: 1 ngoi sao 8 khoang trang
-    //hang 2:3 ngoi sao 6 khoang trang
-    //hang 3:5 ngoi sao 4 khoang trang
-    //hang 4:7 ngoi sao 2 khoang trang
-    
-    
-  return 0;
+int main () {
+int h;
+    printf("nhap h: ");
+    scanf("%d",&h);
+  //nhập tọa độ cao h từ bàn phím
+    for(int i = 1; i <= h; i++) {
+    //cho vòng lặp từ 1 tới h và tăng dần 1 giá trị
+        for(int j = 1; j <= h - i; j++)
+        //vòng lặp bắt đầu từ 1 và kết thúc ở h - i (i tăng dần 1 giá trị). Có nghĩa là khoảng trống sẽ giảm dần.
+            printf("   ");
+            //hiển thị khoảng trống ra màn hình
+        for(int j = 1; j <= 2 * i - 1; j++)
+        //vòng lặp bắt đầu từ 1 và kết thúc ở 2 * i -1. (i tăng dần 1 giá trị). Khi 2 * i -1 thì dấu * sẽ tăng dần theo i
+        //không có cái tam giác nào là 1 2 4 6 8 cả nên phải trừ đi 1 thành 1 3 5 7 9.
+            printf(" * ");
+            // hiển thị dấu * ra màn hình
+    printf("\n");
+    // xuống dòng khi kết thúc 1 lần của vòng lặo
+}
+    return 0;
 }
