7d6f321fc9935dea930217569b465fd101c55c10
tralamcao123 <74332711+tralamcao123@users.noreply.github.com>
2021-01-28 10:38:18 +0700

    Create ham_03.c

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Nhap_pt_mang(int a[])
+{
+  
+
+}
+void Hien_thi_pt_mang(int a[])
+{
+  
+  
+}
+void Hien_thi_tong_chan(int a[])
+{
+ 
+}
+void Dem_chan_le_0(int a[])
+{
+
+}
+
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);  
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
